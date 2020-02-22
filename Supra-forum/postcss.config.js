42ae2411ea087dae19d65a349dfb10b9e8329400
Godfy <1329372653@qq.com>
2020-02-22 19:34:01 +0800

    为了更新（删除）
    为了更新（删除）

--- Supra-forum/postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  plugins: {
-    autoprefixer: {},
-    'postcss-pxtorem': {
-      rootValue: 37.5, //换算基数，一般和html的font-size一致
-      propList: ['*']  //哪些css属性需要换算
-    }
-  }
-}
